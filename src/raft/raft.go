fc824d72375dd305da0cfbf409dc44244ea53a67
Zhe <zhezhou@gatech.edu>
2020-05-07 22:20:24 -0400

    cleared code

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -117,20 +117,20 @@ func (rf *Raft) GetState() (int, bool) {
 	return term, isleader
 }
 
-// Helper function that prints out the log of the server
+// Helper function that prints out the log of the server,
+// and some messages for diagnosing.
 func (rf *Raft) printLog() {
-	// var f string
-	// if rf.isLeader {
-	// 	f = "true"
-	// } else {
-	// 	f = "false"
-	// }
-	// fmt.Printf("The log entries of the %v-th server which is a leader? %v, its commiteIndex is %v and lastApplied is %v, matchIndex is %v \n", rf.me, f, rf.commitIndex, rf.lastApplied, rf.matchIndex)
-	// // for i, l := range rf.log {
-	// // 	fmt.Print(" ", i, ": ")
-	// // 	fmt.Print(l)
-	// // }
-	// fmt.Println("")
+	var f string
+	if rf.isLeader {
+		f = "true"
+	} else {
+		f = "false"
+	}
+	fmt.Printf("The log entries of the %v-th server which is a leader? %v, its commiteIndex is %v and lastApplied is %v, matchIndex is %v \n", rf.me, f, rf.commitIndex, rf.lastApplied, rf.matchIndex)
+	for i, l := range rf.log {
+		fmt.Print(" ", i, ": ", l)
+	}
+	fmt.Println("")
 }
 
 // Helper function that insert a LogStruct to the given index of the lock
@@ -138,12 +138,12 @@ func (rf *Raft) printLog() {
 // E.g. [1,2] insert at index 3 is okay, [1,2] insert at index 4 is bad!
 // Index is the raft index, which is 1-indexed.
 func (rf *Raft) insertLog(index int, command interface{}) {
+	// if this is the case, then we are inserting beyond the possible location
 	if len(rf.log) < index-1 {
 		fmt.Println("Something very wrong in insertLog!!")
-	} else if len(rf.log) == index-1 {
+	} else if len(rf.log) == index-1 { // append
 		rf.log = append(rf.log, &LogStruct{rf.currentTerm, command})
-	} else {
-		fmt.Println("????", rf.log, index)
+	} else { // replace
 		rf.log[index-1] = &LogStruct{rf.currentTerm, command}
 	}
 }
@@ -165,8 +165,6 @@ func (rf *Raft) updateState(state string, targetTerm int) {
 		rf.votedFor = -1
 		if rf.isLeader {
 			rf.isLeader = false
-			// go rf.checkTimeouts()
-			// go func() { rf.checkTimeouts() }()
 		}
 	} else if key == "leader" {
 		rf.isLeader = true
@@ -178,9 +176,8 @@ func (rf *Raft) updateState(state string, targetTerm int) {
 		}
 		// the match index of leader itself is known
 		rf.matchIndex[rf.me] = len(rf.log)
-		// start enhancedHeartBeats()
+		// start heartBeats()
 		go func() { rf.heartBeats() }()
-		// go func() { rf.broadcastEntries(false) }()
 	}
 }
 
@@ -224,7 +221,7 @@ func (rf *Raft) readPersist(data []byte) {
 }
 
 //
-// A goroutine that periodically apply newly commited command to the channel (application)
+// A goroutine that periodically applies newly commited commands to the channel (application).
 // Using Conditional variable, this goroutine will only be waken when signaled.
 func (rf *Raft) applyCommited(applyCh chan ApplyMsg) {
 	for !rf.killed() {
@@ -235,12 +232,9 @@ func (rf *Raft) applyCommited(applyCh chan ApplyMsg) {
 		for rf.commitIndex > rf.lastApplied {
 			// increment lastApplied
 			rf.lastApplied++
-			// fmt.Println(rf.me, "is applying message index", rf.lastApplied, rf.commitIndex, rf.log)
 			// apply log[lastApplied] to state machine
-			// newMsg := ApplyMsg{true, rf.log[rf.lastApplied-1].Command, rf.commitIndex}
 			newMsg := ApplyMsg{true, rf.log[rf.lastApplied-1].Command, rf.lastApplied}
 			applyCh <- newMsg
-			// fmt.Println("&&&&&& ", rf.me, " is commiting", rf.log[rf.lastApplied-1].Command)
 		}
 		rf.mu.Unlock()
 
@@ -297,10 +291,10 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 				lastLogTerm = rf.log[len(rf.log)-1].Term
 			}
 			lastLogIndex := len(rf.log)
-			fmt.Println(rf.me, "is receiving")
+			fmt.Println(rf.me, "is receiving a vote")
 			// if candidate’s log is at least as up-to-date as receiver’s log, grant vote
 			if args.LastLogTerm > lastLogTerm || (args.LastLogTerm == lastLogTerm && args.LastLogIndex >= lastLogIndex) {
-				fmt.Println(rf.me, "is voting")
+				fmt.Println(rf.me, "is voting for some other node")
 				reply.VoteGranted = true
 				rf.votedFor = args.CandidateID
 				// reset timer
@@ -339,7 +333,6 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 	defer rf.mu.Unlock()
 	reply.Term = rf.currentTerm
 	if args.Term < rf.currentTerm {
-		// fmt.Println("@@@@@@@@@@@@@@", args.Term, rf.currentTerm)
 		reply.Success = false
 		return
 	} else if args.Term > rf.currentTerm {
@@ -355,29 +348,12 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 		rf.resetTimer()
 	}
 
-	// reply.Term = rf.currentTerm
-	// reply.Success = true
-
-	// fmt.Println(args.LogEntires)
 	// 2B deal with real log entries
-	// if args.LogEntires != nil && len(args.LogEntires) > 0 {
 	// Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm
-	// fmt.Println("$$$$$$$$$$$")
-	// fmt.Println("&&&&&&", rf.me, args.PrevLogIndex, rf.log)
 	if len(rf.log) < args.PrevLogIndex || (len(rf.log) > 0 && args.PrevLogIndex > 0 && rf.log[args.PrevLogIndex-1].Term != args.PrevLogTerm) {
-		// fmt.Println("**********", rf.me, rf.log, args.PrevLogIndex, args.PrevLogTerm)
 		reply.Success = false
-		// delete conflicting entires and all following it
-		// if len(rf.log) >= args.PrevLogIndex {
-		// 	rf.log = rf.log[:args.PrevLogIndex-1]
-		// }
 	} else {
 		reply.Success = true
-		// test
-		// fmt.Println("((((((", args.LeaderID, rf.me, args.LogEntires)
-		// if len(args.LogEntires) == 0 {
-		// 	return
-		// }
 		// If an existing entry conflicts with a new one (same index but
 		// different terms), delete the existing entry and all that follow it
 		i := 1
@@ -393,33 +369,17 @@ func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply
 			}
 		}
 		// Append any new entries not already in the log
-		// for ; i <= len(args.LogEntires); i++ {
-		// 	toInsertIndex := args.PrevLogIndex + i
-		// 	rf.insertLog(toInsertIndex, args.LogEntires[i-1].Command)
-		// }
 		rf.log = append(rf.log[:args.PrevLogIndex+i-1], args.LogEntires[i-1:]...)
-		// If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
 		if args.LeaderCommit > rf.commitIndex {
-			// oldCommitIndex := rf.commitIndex
-			// rf.commitIndex = min(args.LeaderCommit, len(rf.log))
 			rf.commitIndex = min(args.LeaderCommit, args.PrevLogIndex+len(args.LogEntires))
 			// follower signal commit
-			// for rf.commitIndex > rf.lastApplied && oldCommitIndex < rf.commitIndex {
-			// 	// fmt.Println("!!!!!!!!!", rf.me, rf.isLeader, rf.commitIndex, rf.lastApplied, oldCommitIndex)
-			// 	// rf.mu.Unlock()
-			// 	rf.applyCond.Signal()
-			// 	// time.Sleep(time.Millisecond)
-			// 	// rf.mu.Lock()
-			// 	oldCommitIndex++
-			// }
 			if rf.commitIndex > rf.lastApplied {
 				rf.applyCond.Signal()
 			}
 		}
 	}
 
-	// fmt.Println("leader commit is ", args.LeaderCommit, " commitIndex is ", rf.commitIndex)
-	rf.printLog()
+	// rf.printLog()
 
 }
 
@@ -484,7 +444,6 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	term := -1
 	isLeader := true
 	// Your code here (2B).
-	// fmt.Println(command)
 	rf.mu.Lock()
 	// if not leader, return false immediately
 	if !rf.isLeader {
@@ -499,14 +458,7 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	// apply to leader's local log, should be the next index of the current commitIndex.
 	// If that location is occupied, definitely sends a warning
 
-	// targetCommitIndex := rf.commitIndex + 1
 	targetCommitIndex := len(rf.log) + 1
-
-	// if rf.log[targetCommitIndex] != nil {
-	// 	fmt.Println("Leader's local log already occupied. Some elements get overwritten!!")
-	// }
-
-	// rf.log[targetCommitIndex] = &LogStruct{rf.currentTerm, command}
 
 	rf.insertLog(targetCommitIndex, command)
 
@@ -518,25 +470,10 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	index = targetCommitIndex
 	term = rf.currentTerm
 
-	// return when this is applied to local machine! Wrong! Should return immediately
-	// currentApplied := rf.lastApplied
+	// Should return immediately
 	rf.mu.Unlock()
-	// go func() { rf.broadcastEntries(false) }()
-	// for currentApplied < targetCommitIndex {
-	// 	time.Sleep(time.Millisecond * 20)
-	// 	rf.mu.Lock()	// 	rf.mu.Lock()	// 	rf.mu.Lock()	// 	rf.mu.Lock()
 
-	// 	currentApplied = rf.lastApplied
-	// 	rf.mu.Unlock()
-	// }
-	// fmt.Println("Yes yes yes yes")
-	// fmt.Println(index, term, isLeader)
-	rf.printLog()
-	// rf.mu.Lock()
-	// fmt.Println(">>>>>>>>>>>>>>")
-	// fmt.Println(rf.me, rf.lastApplied, rf.nextIndex, rf.matchIndex)
-	// rf.mu.Unlock() // debug only
-
+	// rf.printLog()
 	return index, term, isLeader
 }
 
@@ -551,22 +488,10 @@ func (rf *Raft) checkTimeouts() {
 	// prevent goroutine leak
 	for !rf.killed() {
 		rf.mu.Lock()
-		// fmt.Println(rf.me, " is checking timeout", "I am a leader?", rf.isLeader)
-		// durationPassed := time.Now().Sub(rf.lastHeardFromLeader).Nanoseconds() / 1e6
 		durationPassed := time.Now().Sub(rf.lastHeardFromLeader)
 		// leader doesn't need checkTimeouts
-		// if rf.isLeader {
-		// 	rf.mu.Unlock()
-		// 	return
-		// } else if durationPassed > rf.timeLimit { // overdue, should start an election
 		if !rf.isLeader && durationPassed > rf.timeLimit { // overdue, should start an election
 			// have a separate goroutine deal with election because checkTimeouts need to keep monitoring
-
-			// debug
-			if rf.isLeader {
-				// fmt.Println("********************")
-			}
-
 			go func() { rf.startElection() }()
 			// reset election timer
 			rf.resetTimer()
@@ -640,7 +565,6 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 		if i == rf.me {
 			continue
 		}
-
 		wg.Add(1)
 
 		targetID := i // save index
@@ -670,15 +594,11 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 				// log entries to apply
 				// rf.printLog()
 				logs := []*LogStruct{}
-				// when heartbeats, send empty logs, else send full logs
-				if !isHeartBeat {
-					// fmt.Println("!!!!!!!!!!", rf.log, nextIndex, rf.nextIndex, rf.isLeader, rf.me)
-				}
+
+				// when heartbeats, send empty log, else send non-empty logs
 				if !isHeartBeat && len(rf.log) > 0 {
 					logs = rf.log[nextIndex-1:]
 				}
-				// fmt.Println(logs)
-				// fmt.Println(rf.isLeader, rf.me, logs, isHeartBeat)
 				// save term
 				recordTerm := rf.currentTerm
 
@@ -699,10 +619,6 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 				}
 				// send RPC call
 				rpcSuccess := rf.sendAppendEntries(targetID, &args, &reply)
-				// heartbeat simply returns
-				// if isHeartBeat {
-				// 	break
-				// }
 
 				// Below is broadcasting real log entries:
 				//
@@ -710,7 +626,6 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 				rf.mu.Lock()
 				// leader out dated
 				if recordTerm != rf.currentTerm {
-					// fmt.Println("Leader ", rf.me, rf.isLeader, " is outdated during sending enhancedHeart beats to server ", targetID, rf.currentTerm)
 					rf.mu.Unlock()
 					break
 				}
@@ -719,7 +634,8 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 					// fmt.Printf("sendAppendEntries RPC is not successful from senderID %v to receiverID %v \n", rf.me, targetID)
 					rf.mu.Unlock()
 				} else {
-					if isSuccess { // If successful: update nextIndex and matchIndex for follower
+					if isSuccess {
+						// If successful: update nextIndex and matchIndex for follower
 						// since success from follower means all logs have been up to date,
 						// we put matchIndex to be the length of current log and nextIndex to be matchIndex + 1
 						// rf.matchIndex[targetID] = len(rf.log)
@@ -729,15 +645,12 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 
 						rf.mu.Unlock()
 					} else if reply.Term > rf.currentTerm { // if the follower has a higher term than the leader, the leader should convert to follower
-						// rf.currentTerm = reply.Term
-						fmt.Println("pppppppppppppp", rf.me, targetID, rf.currentTerm, reply.Term, rf.isLeader)
 						rf.updateState("follower", reply.Term)
 						rf.mu.Unlock()
-						// go func() { rf.checkTimeouts() }()
 						// escape the check loop
 						break
-					} else { // because of log inconsistency, decrement nextIndex and retry
-						// fmt.Println("%%%%%%%%%% ", reply.Term, rf.currentTerm)
+					} else {
+						// because of log inconsistency, decrement nextIndex and retry
 						rf.nextIndex[targetID]--
 						rf.mu.Unlock()
 					}
@@ -763,7 +676,6 @@ func (rf *Raft) broadcastEntries(isHeartBeat bool) {
 					sum += m[N]
 					// if exist a majority of matchIndex[i] ≥ N,
 					// and log[N].term == currentTerm, set commitIndex = N
-					// fmt.Println("SumSumSum", rf.me, rf.isLeader, sum, m)
 					if sum > len(rf.peers)/2 && rf.log[N-1].Term == rf.currentTerm {
 						rf.commitIndex = N
 						rf.applyCond.Signal()
@@ -790,11 +702,8 @@ func (rf *Raft) startElection() {
 	rf.mu.Lock()
 	// Increment current Term
 	rf.currentTerm++
-	// fmt.Println(rf.me, rf.isLeader, " is starting the election on term ", rf.currentTerm, rf.log, rf.matchIndex, rf.nextIndex)
 	// vote for yourself
 	rf.votedFor = rf.me
-	// // clear voted for
-	// rf.votedFor = -1
 
 	// record current term that the election starts
 	// If later this election time out then this term would be less than rf.currentTerm,
@@ -836,14 +745,12 @@ func (rf *Raft) startElection() {
 			if !rpcSuccess {
 				// fmt.Printf("sendRequestVote RPC is not successful from senderID %v to receiverID %v \n", rf.me, targetID)
 			} else {
-				// successful rpc, process result here
-
-				// if the terms are not equal, return
+				// successful rpc, process result here.
+				// if the terms are not equal, return.
 				if rf.currentTerm != recordTerm {
 					rf.mu.Unlock()
 					return
 				}
-
 				// if the server vote for you
 				if voteReply.VoteGranted {
 					electionLock.Lock()
@@ -851,7 +758,7 @@ func (rf *Raft) startElection() {
 					// election succeed, should process here because we don't want blocking(unreachable) rpc to halt the whole election
 					if majorityCount >= threshold && rf.currentTerm == recordTerm && !rf.isLeader {
 						// change state to leader
-						rf.updateState("leader", -99999) // term is not used
+						rf.updateState("leader", -99999) // the term parameter is not used for update leader
 					}
 					electionLock.Unlock()
 				} else {
@@ -948,9 +855,6 @@ func Make(peers []*labrpc.ClientEnd, me int,
 
 	// start checktimeouts
 	go func() { rf.checkTimeouts() }()
-
-	//
-	// go func() { rf.heartBeats() }()
 
 	// start applying commited messages
 	go func() { rf.applyCommited(applyCh) }()
